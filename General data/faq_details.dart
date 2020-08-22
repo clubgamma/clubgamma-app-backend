d4f5447dd8189b51e13c5521ae2c2d960e6aa994
Harsh Patel <harshptl714@gmail.com>
2020-08-23 02:20:53 +0530

    Create faq_details.dart

---
+++ General data/faq_details.dart
@@ -0,0 +1,8 @@
+- name: Intro to git & GitHub
+  detail: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+- name: Is Club Gamma a profitable organization?
+  detail: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+- name: Is there also a website for ClubGamma?
+  detail: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+- name: Why Club Gamma?
+  detail: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
